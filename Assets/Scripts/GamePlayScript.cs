3276f4624308f64e48a81495e98df7068882bc32
ProvakarRoy <sunhik.10pr@gmail.com>
2022-07-15 18:20:16 +0530

    2nd push

--- Assets/Scripts/GamePlayScript.cs
+++ Assets/Scripts/GamePlayScript.cs
@@ -8,28 +8,43 @@ public class GamePlayScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
+   /* [SerializeField]
+    private GameObject ring1;
     [SerializeField]
-    private GameObject ring;
+    private GameObject ring2;
+    [SerializeField]
+    private GameObject ring3;*/
     [SerializeField]
     private GameObject points;
     [SerializeField]
     private Text showScore;
     [SerializeField]
     private GameObject gameOverText;
+    [SerializeField]
+    private GameObject[] ringPrefab = new GameObject[3];
     public List<GameObject> EnemyPrefabs = new List<GameObject>();
 
     private bool isGameOver;
-    private float X_position = -2.0f;
-    private float Y_position = 4.6f;
+    private float ringSpawnDis;
+    private GameObject ring;
+
+    private float X_position=0;
+    private float Y_position=0;
     private int ran=0;
     private int val=0;
     private int score = 0;
+    private bool isMoving=false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         isGameOver = false;
+        ring = ringPrefab[0];
+        /*for(int i=0; i < EnemyPrefabs.Count; i++)
+        {
+            Instantiate(EnemyPrefabs[i], new Vector3(100, 100, 100), Quaternion.identity);
+        }*/
     }
 
     // Update is called once per frame
@@ -37,10 +52,15 @@ public class GamePlayScript : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && isGameOver == false)
         {
+            isMoving = true;
+        }
+        if((isMoving==true) && isGameOver == false)
+        {
             player.transform.position = Vector3.MoveTowards(player.transform.position, ring.transform.position, 5f * Time.deltaTime);
         }
         if(player.transform.position == ring.transform.position && isGameOver == false)
         {
+            isMoving = false;
             EnemyPrefabs[ran].transform.position = new Vector3(100, 100, 100);
             val = Random.Range(0, 2);
             repeatRing(val);
@@ -56,7 +76,11 @@ public class GamePlayScript : MonoBehaviour
 
     public void repeatRing(int spawnValue)
     {
-        Y_position = Y_position + 9.2f;
+        ring = ringPrefab[Random.Range(0, 3)];
+        ringSpawnDis = Mathf.Round(Random.Range(-2.0f, 2.5f));
+        Debug.Log(ringSpawnDis);
+        Y_position = Y_position + Random.Range(1, 5);
+        X_position =  ringSpawnDis;
         ring.transform.position= new Vector3(X_position, Y_position, ring.transform.position.z);
         ran=Random.Range(0,EnemyPrefabs.Count);
         EnemyPrefabs[ran].transform.position = new Vector3(X_position, Y_position, ring.transform.position.z);
